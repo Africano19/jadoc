f9dcb8d4669eb91b8e6c2d8a70344dc450e12c8b
Africano19 <81010248+Africano19@users.noreply.github.com>
2022-05-11 12:07:14 +0100

    .

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 session_start();
-$host = "cp175.webserver.pt"; /* Host name */
-$user = "jadocpt_teste"; /* User */
-$password = "Jadoc2022!"; /* Password */
-$dbname = "jadocpt_teste"; /* Database name */
 
-$con = mysqli_connect($host, $user, $password,$dbname);
-// Check connection
-if (!$con) {
-  die("Connection failed: " . mysqli_connect_error());
-}
+  $host = "ec2-34-246-227-219.eu-west-1.compute.amazonaws.com";
+  $dbname = "d7qhm5em88onq7";
+  $user = "gfbxhdxaeztipf";
+  $password = "9b941ecbebb7e4952fdd5e18d0e675449cd04ea33def9da26d77ae84358d0574";
+  $port = "5432";
+
+  $con = "dbname=" . $dbname . " host=" . $host . " port=" . $port . " user=" . $user . " password=" . $password . " sslmode=require";
+
+  $conn = pg_connect($con);
 
 ?>
