63b9fc7050162c6a75ed6acc9b3cc391eff6ece7
Africano19 <81010248+Africano19@users.noreply.github.com>
2022-05-11 12:31:04 +0100

    .

--- config.php
+++ config.php
@@ -1,14 +1,20 @@
 <?php
 session_start();
 
-  $host = "ec2-34-246-227-219.eu-west-1.compute.amazonaws.com";
-  $dbname = "d7qhm5em88onq7";
-  $user = "gfbxhdxaeztipf";
-  $password = "9b941ecbebb7e4952fdd5e18d0e675449cd04ea33def9da26d77ae84358d0574";
-  $port = "5432";
+$host = "ec2-34-246-227-219.eu-west-1.compute.amazonaws.com";
+$dbname = "d7qhm5em88onq7";
+$user = "gfbxhdxaeztipf";
+$password = "9b941ecbebb7e4952fdd5e18d0e675449cd04ea33def9da26d77ae84358d0574";
+$port = "5432";
 
-  $con = "dbname=" . $dbname . " host=" . $host . " port=" . $port . " user=" . $user . " password=" . $password . " sslmode=require";
+$con = "dbname=" . $dbname . " host=" . $host . " port=" . $port . " user=" . $user . " password=" . $password . " sslmode=require";
 
-  $conn = pg_connect($con);
+$conn = pg_connect($con);
+
+if($conn){
+    echo "Connected <br />" . $conn;
+}else {
+    echo "Not connected";
+}
 
 ?>
